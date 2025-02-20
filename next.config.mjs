4a55510e3dbe1c68e0cd3aa2c90c808403e478ee
geUrz <gerardourzua20@gmail.com>
2025-02-19 17:29:22 -0800

    Start16

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Manténlo en desarrollo para detectar errores
+  reactStrictMode: false, // Manténlo en desarrollo para detectar errores
   images: {
     domains: ['localhost'], // Permitir imágenes desde localhost
   },
