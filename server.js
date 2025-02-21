fc30781f407d10cfe3733e4f55866a63199c0882
geUrz <gerardourzua20@gmail.com>
2025-02-20 18:42:10 -0800

    Start25

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require('path')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+console.log('NODE_ENV:', process.env.NODE_ENV);
 
 app.prepare().then(() => {
   const server = express()
